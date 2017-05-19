b973c231c229925ba2e4424d38e15c8e14539339
chensenwei <chensenwei@meituan.com>
2017-05-19 11:24:23 +0800

    v1

--- v1/js/index.js
+++ v1/js/index.js
@@ -1,9 +1,31 @@
 function SelfVue (data, el, exp) {
+    var self = this;
     this.data = data;
+
+    Object.keys(data).forEach(function(key) {
+        self.proxyKeys(key);
+    });
+
     observe(data);
     el.innerHTML = this.data[exp];  // 初始化模板数据的值
     new Watcher(this.data, exp, function (value) {
         el.innerHTML = value;
     });
     return this;
+}
+
+SelfVue.prototype = {
+    proxyKeys: function (key) {
+        var self = this;
+        Object.defineProperty(this, key, {
+            enumerable: false,
+            configurable: true,
+            get: function proxyGetter() {
+                return self.data[key];
+            },
+            set: function proxySetter(newVal) {
+                self.data[key] = newVal;
+            }
+        });
+    }
 }
